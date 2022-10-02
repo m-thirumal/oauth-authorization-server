28466a3e3cf12ae72ab5c456b72e15f2e498a60d
Thirumal <m.thirumal@hotmail.com>
2022-10-02 07:07:14 +0530

    Sample

--- src/main/java/in/thirumal/config/AuthorizationServerConfig.java
+++ src/main/java/in/thirumal/config/AuthorizationServerConfig.java
@@ -5,7 +5,6 @@ package in.thirumal.config;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
@@ -18,7 +17,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
@@ -29,7 +27,9 @@ import org.springframework.security.oauth2.server.authorization.config.ProviderS
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 /**
  * @author Thirumal
@@ -72,24 +72,40 @@ public class AuthorizationServerConfig {
 	    return http.formLogin(Customizer.withDefaults()).build();
 	}
 	
+	@Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        RSAKey rsaKey = generateRsa();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
 
-	    private static KeyPair generateRsaKey() {
-	        KeyPair keyPair;
-	        try {
-	            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-	            keyPairGenerator.initialize(2048);
-	            keyPair = keyPairGenerator.generateKeyPair();
-	        } catch (Exception ex) {
-	            throw new IllegalStateException(ex);
-	        }
-	        return keyPair;
-	    }
+    private static RSAKey generateRsa() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey)
+          .privateKey(privateKey)
+          .keyID(UUID.randomUUID().toString())
+          .build();
+    }
 
-	    @Bean
-	    public ProviderSettings providerSettings() {
-	        return ProviderSettings.builder()
-	          .issuer("http://auth-server:9000")
-	          .build();
-	    }
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+
+    @Bean
+    public ProviderSettings providerSettings() {
+        return ProviderSettings.builder()
+          .issuer("http://127.0.0.1:9000")
+          .build();
+    }
 	
 }
