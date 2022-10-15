64110e51d4e9a057c84110647bd897227fd7fc89
Thirumal <m.thirumal@hotmail.com>
2022-10-15 07:50:39 +0530

    Added filter

---
+++ src/main/java/in/thirumal/filter/RequestResponseLoggingFilter.java
@@ -0,0 +1,46 @@
+package in.thirumal.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(2)
+public class RequestResponseLoggingFilter implements Filter {
+
+	private final static Logger LOG = LoggerFactory.getLogger(RequestResponseLoggingFilter.class);
+
+	@Override
+	public void init(final FilterConfig filterConfig) throws ServletException {
+		LOG.info("Initializing filter :{}", this);
+	}
+
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		LOG.info("Logging Request  {} : {}", req.getMethod(), req.getRequestURI());
+		chain.doFilter(request, response);
+		LOG.info("Logging Response :{}", res.getContentType());
+	}
+	
+	@Override
+	public void destroy() {
+		LOG.warn("Destructing filter :{}", this);
+	}
+
+}
