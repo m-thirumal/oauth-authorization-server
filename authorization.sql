725ef2b96cf370377713d2534d95e23ae48af53e
Thirumal <m.thirumal@hotmail.com>
2023-02-12 16:34:50 +0530

    A

--- authorization.sql
+++ authorization.sql
@@ -1,5 +1,5 @@
 -- Database generated with pgModeler (PostgreSQL Database Modeler).
--- pgModeler version: 1.0.0-beta
+-- pgModeler version: 1.0.0
 -- PostgreSQL version: 15.0
 -- Project Site: pgmodeler.io
 -- Model Author: Thirumal
@@ -18,10 +18,11 @@ COMMENT ON DATABASE "authorization" IS E'Created Thirumal';
 -- object: public.oauth2_authorization | type: TABLE --
 -- DROP TABLE IF EXISTS public.oauth2_authorization CASCADE;
 CREATE TABLE public.oauth2_authorization (
-	id serial NOT NULL,
+	id varchar(100) NOT NULL,
 	registered_client_id varchar(100) NOT NULL,
 	principal_name varchar(200) NOT NULL,
 	authorization_grant_type varchar(100) NOT NULL,
+	authorized_scopes varchar(1000),
 	attributes text,
 	state varchar(500),
 	authorization_code_value text,
