69cc6da36a9237f113845ec589dd32b41fea5f50
Thirumal <m.thirumal@hotmail.com>
2023-02-23 23:05:43 +0530

    Working code of register client

--- src/main/java/in/thirumal/service/RegisterClientService.java
+++ src/main/java/in/thirumal/service/RegisterClientService.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
@@ -30,6 +31,9 @@ public class RegisterClientService {
 	JdbcTemplate jdbcTemplate;
 	
 	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
 	RegisteredClientRepository registeredClientRepository;
 	
 	public RegisteredClient register(Client client) {
@@ -42,17 +46,18 @@ public class RegisterClientService {
 				RegisteredClient
 				.withId(clientPrimaryKey)
 				.clientId(UUID.randomUUID().toString())
-		        .clientSecret("{noop}"+ UUID.randomUUID().toString())
-		        .clientAuthenticationMethods(t -> client.getClientAuthenticationMethods())
+		       // .clientSecret("{noop}"+ UUID.randomUUID().toString())
+				.clientSecret(passwordEncoder.encode("Thirumal"))
+		        .clientAuthenticationMethods(t -> t.addAll(client.getClientAuthenticationMethods()))
 		      //   . clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-		        .authorizationGrantTypes(t -> client.getAuthorizationGrantTypes())
+		        .authorizationGrantTypes(t -> t.addAll(client.getAuthorizationGrantTypes()))
 		       //   .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 		        //  .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
 		          //.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-		        .redirectUris(r -> client.getRedirectUris()) 
+		        .redirectUris(r -> r.addAll(client.getRedirectUris())) 
 		      //  .redirectUri("http://127.0.0.1:8000/login/oauth2/code/users-client-oidc")
 		         // .redirectUri("http://127.0.0.1:8000/authorized")
-		        .scopes(s -> client.getScopes())
+		        .scopes(s -> s.addAll(client.getScopes()))
 		          //.scope(OidcScopes.OPENID)
 		          //.scope(OidcScopes.PROFILE)
 		          //.scope("read")
