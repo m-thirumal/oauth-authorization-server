92ee1ac65c89cf86afb5774c3e18dc9d619e51e3
Thirumal <m.thirumal@hotmail.com>
2023-02-25 17:30:52 +0530

    Fully Working code

--- src/main/java/in/thirumal/config/DefaultSecurityConfig.java
+++ src/main/java/in/thirumal/config/DefaultSecurityConfig.java
@@ -3,26 +3,23 @@
  */
 package in.thirumal.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * @author Thirumal
  *
  */
 @EnableWebSecurity
-//@Configuration(proxyBeanMethods = false)
+@Configuration(proxyBeanMethods = false)
 public class DefaultSecurityConfig {
 
-	@Bean
-	//@Order(2)
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()).formLogin(Customizer.withDefaults());
-		return http.build();
-	}
+//	@Bean
+//	@Order(2)
+//	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()).formLogin(withDefaults());
+//		return http.build();
+//	}
 	
 //	@Bean
 //	UserDetailsService users() {
