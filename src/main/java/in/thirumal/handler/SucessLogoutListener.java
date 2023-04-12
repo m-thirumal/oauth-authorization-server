027ef06b9dd18a7d6731b27d0a6a47f8725cc1b3
Thirumal <m.thirumal@hotmail.com>
2023-04-12 08:23:15 +0530

    Logout

--- src/main/java/in/thirumal/handler/SucessLogoutListener.java
+++ src/main/java/in/thirumal/handler/SucessLogoutListener.java
@@ -3,39 +3,26 @@
  */
 package in.thirumal.handler;
 
-import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.LogoutSuccessEvent;
 import org.springframework.stereotype.Component;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * @author Thirumal
  */
 @Component
-public class SucessLogoutListener implements LogoutSuccessHandler{
+public class SucessLogoutListener implements ApplicationListener<LogoutSuccessEvent>{
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-//
-//	@Override
-//	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-//			Authentication authentication) throws IOException, ServletException {
-//		System.out.println(authentication);
-//		logger.debug("Err ");
-//	}
-
 
 	@Override
-	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-			throws IOException, ServletException {
-		logger.debug("Log out {}", authentication);
+	public void onApplicationEvent(LogoutSuccessEvent event) {
+		logger.debug("Logout Success event : {}", event);
+		String userName  = event.getAuthentication().getName();
+		System.out.println(userName);
 	}
 
 }
