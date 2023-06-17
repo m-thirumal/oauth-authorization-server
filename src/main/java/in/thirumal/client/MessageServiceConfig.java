2ea6485ad78fa78d3b5dbdeb2def02a66005028b
Thirumal <m.thirumal@hotmail.com>
2023-06-17 21:48:50 +0530

    Bean

--- src/main/java/in/thirumal/client/MessageServiceConfig.java
+++ src/main/java/in/thirumal/client/MessageServiceConfig.java
@@ -6,6 +6,7 @@ package in.thirumal.client;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import feign.Logger;
 import feign.Request;
@@ -14,15 +15,16 @@ import feign.Request;
  * @author Thirumal
  *
  */
+@Configuration
 public class MessageServiceConfig {
 
-	@Bean
-    public Logger.Level feignLoggerLevel() {
+    @Bean
+    Logger.Level feignLoggerLevel() {
         return Logger.Level.BASIC;
     }
-	
-	@Bean
-	public Request.Options notificationOptions() {
+
+    @Bean
+    Request.Options notificationOptions() {
 		return new Request.Options(6, TimeUnit.MINUTES, 6, TimeUnit.MINUTES, true);
 	}
 	
